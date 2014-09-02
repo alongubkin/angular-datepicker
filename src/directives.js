e7fc74ffb54a830396126d8b2260c57afaa0b2e9
Jonathan Bowers <jonotron@gmail.com>
2014-09-02 14:15:21 -0700

    pickadate options supports user supplied onClose and onSet

--- src/directives.js
+++ src/directives.js
@@ -11,43 +11,57 @@ angular.module('angular-datepicker', [])
 	        link: function (scope, element, attrs) {
 	            var options = scope.pickADateOptions || {};
 
+                var userOnSet = options.onSet;
+                function onSet (e) {
+                    if (typeof userOnSet === 'function') {
+                        userOnSet.apply(this, arguments);
+                    }
+
+                    if (scope.$$phase || scope.$root.$$phase) // we are coming from $watch or link setup
+                        return;
+                    var select = element.pickadate('picker').get('select'); // selected date
+                    scope.$apply(function () {
+                        if (e.hasOwnProperty('clear')) {
+                            scope.pickADate = null;
+                            return;
+                        }
+                        if (!scope.pickADate || typeof scope.pickADate === 'string')
+                            scope.pickADate = new Date(0);
+                        scope.pickADate.setYear(select.obj.getYear() + 1900); // hello Y2K...
+                        // It took me half a day to figure out that javascript Date object's getYear
+                        // function returns the years since 1900. Ironically setYear() accepts the actual year A.D.
+                        // So as I got the $#%^ 114 and set it, guess what, I was transported to ancient Rome 114 A.D.
+                        // That's it I'm done being a programmer, I'd rather go serve Emperor Trajan as a sex slave.
+                        scope.pickADate.setMonth(select.obj.getMonth());
+                        scope.pickADate.setDate(select.obj.getDate());
+                    });
+                }
+
+                var userOnClose = options.onClose;
+                function onClose (e) {
+                    if (typeof userOnClose === 'function') {
+                        userOnClose.apply(this, arguments);
+                    }
+
+                    if (typeof cordova === 'undefined' || !cordova.plugins || !cordova.plugins.Keyboard) {
+                        return;
+                    }
+
+                    var keyboardShowCallback = function () {
+                        cordova.plugins.Keyboard.close();
+                        window.removeEventListener('native.keyboardshow', this);
+                    };
+
+                    window.addEventListener('native.keyboardshow', keyboardShowCallback);
+
+                    setTimeout(function () {
+                        window.removeEventListener('native.keyboardshow', keyboardShowCallback);
+                    }, 500);
+                }
+
 	            element.pickadate(angular.extend(options, {
-	                onSet: function (e) {
-	                    if (scope.$$phase || scope.$root.$$phase) // we are coming from $watch or link setup
-	                        return;
-	                    var select = element.pickadate('picker').get('select'); // selected date
-	                    scope.$apply(function () {
-	                        if (e.hasOwnProperty('clear')) {
-	                            scope.pickADate = null;
-	                            return;
-	                        }
-	                        if (!scope.pickADate || typeof scope.pickADate === 'string')
-	                            scope.pickADate = new Date(0);
-	                        scope.pickADate.setYear(select.obj.getYear() + 1900); // hello Y2K...
-	                        // It took me half a day to figure out that javascript Date object's getYear
-	                        // function returns the years since 1900. Ironically setYear() accepts the actual year A.D.
-	                        // So as I got the $#%^ 114 and set it, guess what, I was transported to ancient Rome 114 A.D.
-	                        // That's it I'm done being a programmer, I'd rather go serve Emperor Trajan as a sex slave.
-	                        scope.pickADate.setMonth(select.obj.getMonth());
-	                        scope.pickADate.setDate(select.obj.getDate());
-	                    });
-	                },
-	                onClose: function () {
-						if (typeof cordova === 'undefined' || !cordova.plugins || !cordova.plugins.Keyboard) {
-							return;
-						}
-
-						var keyboardShowCallback = function () {
-							cordova.plugins.Keyboard.close();
-							window.removeEventListener('native.keyboardshow', this);
-						};
-
-						window.addEventListener('native.keyboardshow', keyboardShowCallback);
-
-						setTimeout(function () {
-							window.removeEventListener('native.keyboardshow', keyboardShowCallback);
-						}, 500);
-	                },
+	                onSet: onSet,
+	                onClose: onClose,
 					container: document.body
 	            }));
 
@@ -67,43 +81,57 @@ angular.module('angular-datepicker', [])
 	            pickATimeOptions: '='
 	        },
 	        link: function (scope, element, attrs) {
-	            var options = scope.pickADateOptions || {};
+	            var options = scope.pickATimeOptions || {};
+
+                var userOnSet = options.onSet;
+                function onSet (e) {
+                    if (typeof userOnSet === 'function') {
+                        userOnSet.apply(this, arguments);
+                    }
+
+                    if (scope.$$phase || scope.$root.$$phase) // we are coming from $watch or link setup
+                        return;
+                    var select = element.pickatime('picker').get('select'); // selected date
+                    scope.$apply(function () {
+                        if (e.hasOwnProperty('clear')) {
+                            scope.pickATime = null;
+                            return;
+                        }
+                        if (!scope.pickATime || typeof scope.pickATime === 'string')
+                            scope.pickATime = new Date();
+
+                        scope.pickATime.setHours(select.hour);
+                        scope.pickATime.setMinutes(select.mins);
+                        scope.pickATime.setSeconds(0);
+                        scope.pickATime.setMilliseconds(0);
+                    });
+                }
+
+                var userOnClose = options.onClose;
+                function onClose (e) {
+                    if (typeof userOnClose === 'function') {
+                        userOnClose.apply(this, arguments);
+                    }
+
+                    if (typeof cordova ==='undefined' || !cordova.plugins || !cordova.plugins.Keyboard) {
+                        return;
+                    }
+                    
+                    var keyboardShowCallback = function () {
+                        cordova.plugins.Keyboard.close();
+                        window.removeEventListener('native.keyboardshow', this);
+                    };
+
+                    window.addEventListener('native.keyboardshow', keyboardShowCallback);
+                    
+                    setTimeout(function () {
+                        window.removeEventListener('native.keyboardshow', keyboardShowCallback);
+                    }, 500);
+                }
 
 	            element.pickatime(angular.extend(options, {
-	                onSet: function (e) {
-	                    if (scope.$$phase || scope.$root.$$phase) // we are coming from $watch or link setup
-	                        return;
-	                    var select = element.pickatime('picker').get('select'); // selected date
-	                    scope.$apply(function () {
-	                        if (e.hasOwnProperty('clear')) {
-	                            scope.pickATime = null;
-	                            return;
-	                        }
-	                        if (!scope.pickATime || typeof scope.pickATime === 'string')
-	                            scope.pickATime = new Date();
-
-	                        scope.pickATime.setHours(select.hour);
-	                        scope.pickATime.setMinutes(select.mins);
-	                        scope.pickATime.setSeconds(0);
-	                        scope.pickATime.setMilliseconds(0);
-	                    });
-	                },
-	                onClose: function () {
-						if (typeof cordova ==='undefined' || !cordova.plugins || !cordova.plugins.Keyboard) {
-							return;
-						}
-						
-						var keyboardShowCallback = function () {
-							cordova.plugins.Keyboard.close();
-							window.removeEventListener('native.keyboardshow', this);
-						};
-
-						window.addEventListener('native.keyboardshow', keyboardShowCallback);
-						
-						setTimeout(function () {
-							window.removeEventListener('native.keyboardshow', keyboardShowCallback);
-						}, 500);
-	                },
+	                onSet: onSet,
+	                onClose: onClose,
 					container: document.body
 	            }));
 
