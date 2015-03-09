4ed4b2f86e311358b86a919021a9ceb26f84c2f8
mestremuten <xcrash1984@gmail.com>
2015-03-09 17:32:52 +0100

    Update picker.js

--- src/picker.js
+++ src/picker.js
@@ -610,7 +610,7 @@ function PickerConstructor( ELEMENT, NAME, COMPONENT, OPTIONS ) {
 	
 		// When something within the root holder is clicked, stop it
         // from bubbling to the doc.
-        P.$root.on('click', function( event ) {
+        P.$root.on('mousedown click', function( event ) {
 			
 			var target = event.target
 
@@ -631,8 +631,8 @@ function PickerConstructor( ELEMENT, NAME, COMPONENT, OPTIONS ) {
 					// from elements focused within the picker.
 					ELEMENT.focus()
 				}
-			} else if ( P.get('open') ) {
-				P.close()
+			} else if ( event.type == 'click' && P.get('open') ) {
+				P.close();
 			}
 		});
 
